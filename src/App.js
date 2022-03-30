452c5763db91567b734eaf40f6651a4daf728688
Rahmad Alamsyah Nazaruddin <54619997+Alamnzr123@users.noreply.github.com>
2022-03-30 12:13:22 +0700

    Todos_ReactJS_4

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Todohome from './Component/Todohome';
+
+function App() {
+  return (
+    <div>
+    <Todohome></Todohome>
+  </div>
+  );
+}
+
+export default App;
